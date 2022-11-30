4a3ee287232beaa253612823f77c5461cc955d7d
Bjork Weenk <bjorkweenk.bw@gmail.com>
2022-11-30 13:30:26 +0100

    footer/nav

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './Footer.css'
 
+
 class Footer extends Component {
     render() {
         return (
-            <div>
-             <h1> Footer </h1>   
-            </div>
+            <>
+            <footer>
+                    <div className="img">
+                       <a href="./home"> <img className="./reuvers.jpg" src='../logobw.png' width={50} height={50}></img></a>
+                        <span className="rights"> All rights Reuvers 2022© </span>
+                    </div>
+            </footer>
+        </>
         );
     }
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,28 +5,13 @@ class Navbar extends Component {
     render() {
         return (
 
-            <div>
-                <header>
-                    <nav>
-                    <ul>
-              <li><a href="About"> Reuvers</a></li>
-              <div class="dropdown">
-                            <button class="dropbtn">Diensten</button>
-                            <div class="dropdown-content">
-                                <a href="#">Bomen rooien</a>
-                                <a href="#">Bomen snoeien</a>
-                                <a href="#">Bomen verzorging</a>
-                                <a href="#">Bomenstronken verwijderen</a>
-                                <a href="#">Tuin aanleg en onderhouden </a>
-                                <a href="#"> Gazon aanleg en verzorging</a>
-                            </div>
-                        </div>
-              <li><a href="./contact">Contact</a></li>
-
-                        </ul>
-
-                    </nav>
-                </header>
+            <div id="navbar">
+                <ul class="nav-bar">
+                    <li class="item-nav"><a class="item-nav" href="/">LOGO</a>  </li>
+                    <li class="item-nav"><a class="item-nav" href="diensten"> Diensten </a></li>
+                    <li class="item-nav"><a class="item-nav" href="reuvers" >Reuvers </a></li>
+                    <li class="item-nav"><a class="item-nav" href="contact">Contact </a> </li>
+                </ul>
             </div>
         );
     }
